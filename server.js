00222ffc3f2a5218892bc61f8b4653649e62b36b
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-05 10:31:56 +0100

    connect server file to socket.io

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Run when a client connects
+io.on('connection', () => {
+  console.log('New Websocket connection');
+});
+
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
 });
